6d55fe05b6f1b9c118b33aef7b7f76b0ceb8b176
Namyts <35004248+Namyts@users.noreply.github.com>
2020-11-25 13:08:41 +0000

    updated add-aliases-to-hosts script to also add aliases to the coredns config map and restart the pod

--- add-aliases-to-hosts.sh
+++ add-aliases-to-hosts.sh
@@ -1,10 +1,23 @@
-cat /etc/hosts | grep -v \#WSL2Alias | sudo tee /etc/hosts &> /dev/null
+hostsFile=/etc/hosts
+cat $hostsFile | grep -v \#WSL2Alias | sudo tee $hostsFile &> /dev/null
+
+corednsConfigMapFile=~/evn-kubernetes-config/overlays/local/resources/coredns-configmap.yaml
+cat $corednsConfigMapFile | grep -v \#WSL2Alias | sudo tee $corednsConfigMapFile &> /dev/null
 
 aliasString=`cat /home/namyts/my-wsl-setup/wsl2aliases/aliases`
+ip=$(ifconfig enp0s3 | awk '{print $2}' | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}")
 
-#echo $aliasString
+CoreDNSLineNumber=$(awk '/custom\.hosts/{ print NR; exit }' ~/evn-kubernetes-config/overlays/local/resources/coredns-configmap.yaml)
+
+CoreDNSLineNumber=$(($CoreDNSLineNumber+1))
 
 read -a arr <<< $aliasString
 for a in "${arr[@]}"; do
-	echo -e "127.0.0.1	$a #WSL2Alias" | sudo tee -a /etc/hosts &> /dev/null
+	text="$ip	$a #WSL2Alias"
+	tabbyText="          "$text
+	echo -e "$text" | sudo tee -a $hostsFile &> /dev/null
+	sed -i "${CoreDNSLineNumber}i\\${tabbyText}" $corednsConfigMapFile #&> /dev/null
 done
+
+pod=$(kubectl get pods -n kube-system | awk '{print $1}' | grep coredns)
+kubectl delete pod -n kube-system $pod
