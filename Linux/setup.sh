f2638ce562816e1bb0219353d0a90bff8cde3c48
namyts <35004248+Namyts@users.noreply.github.com>
2021-03-05 11:43:47 +0000

    added open-iscsi installation

--- Linux/setup.sh
+++ Linux/setup.sh
@@ -132,3 +132,7 @@ helm repo update
 helm upgrade -i helm-operator fluxcd/helm-operator \
     --namespace flux \
     --set helm.versions=v3
+
+#for openebs to work
+sudo apt-get install -y open-iscsi
+sudo systemctl enable --now iscsid
