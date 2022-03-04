51e682a69e46043357eb33c9a1f1538a896a0692
namyts <35004248+Namyts@users.noreply.github.com>
2022-03-04 10:10:38 +0000

    disable wayland and added port to ports

--- Windows/ssh-pf.cmd
+++ Windows/ssh-pf.cmd
@@ -3,7 +3,7 @@ SETLOCAL EnableDelayedExpansion
 
 set sshcommand=%*
 
-set "ports=30000 30001 30002 30003 30004 30005 30006 30007 30008 30009"
+set "ports=30000 30001 30002 30003 30004 30005 30006 30007 30008 30009 30101"
 set "portforward="
 (for %%p in (%ports%) do (
 	set "portforward=!portforward! -L %%p:127.0.0.1:%%p"
