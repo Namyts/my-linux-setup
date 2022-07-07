2889dad6889f6ab71010ed8675f5e1891cc10174
Namyts <35004248+Namyts@users.noreply.github.com>
2022-07-07 10:42:19 +0100

    updated load-cmd

--- Windows/load-cmd.cmd
+++ Windows/load-cmd.cmd
@@ -1,4 +1,15 @@
 @echo off
-IF %CD% == C:\Windows\system32 (
-	cd C:\Users\james
-)
+IF %CD% == C:\Windows\System32 ( GOTO CD )
+IF %CD% == C:\Windows\system32 ( GOTO CD )
+IF %CD% == C:\windows\System32 ( GOTO CD )
+IF %CD% == C:\windows\system32 ( GOTO CD )
+IF %CD% == C:\WINDOWS\System32 ( GOTO CD )
+IF %CD% == C:\WINDOWS\system32 ( GOTO CD )
+
+IF %CD% == C:\WINDOWS\SYSTEM32 ( GOTO CD )
+
+GOTO DONE
+:CD
+cd C:\Users\james
+
+:DONE
